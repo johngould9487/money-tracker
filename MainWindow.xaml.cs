c9273fd6713fe7d0f6fb74cd9ec168cbc32d669c
John Gould <john.gould@anexsys.com>
2019-10-13 18:58:56 +0100

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace money_tracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataTable table = new DataTable();
+            table.Columns.Add("Forename", typeof(string));
+            table.Columns.Add("Surname", typeof(string));
+            table.Columns.Add("Age", typeof(int));
+            DataRow row = table.NewRow();
+            row["Forename"] = "John";
+            row["Surname"] = "Gould";
+            row["Age"] = 28;
+            table.Rows.Add(row);
+            myTable.DataContext = table;
+        }
+    }
+}
